30189318ffd3c87eefed40d5c881ea2b6afa3f95
Sarah <sarahdomson26@gmail.com>
2024-12-04 21:27:52 +0000

    Image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection";
+import Avatar from './assets/Avatar.jpg';
 
 function App() {
   return (
     <div>
       <HeroSection />
+      <img src={Avatar} alt="Avatar" />
     </div>
   );
 }
