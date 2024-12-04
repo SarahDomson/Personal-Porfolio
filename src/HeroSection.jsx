7a78ecd827961ee34e6ec218866d046ae705f80b
Sarah <sarahdomson26@gmail.com>
2024-12-04 21:20:48 +0000

    Updated file

---
+++ src/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <body className="bg-black w-screen h-screen p-5">
+    <main className="max-w-7xl mx-auto">
+       <header>
+        <a className="text-lg font-semibold flex justify-end text-[#cd7f32] font-lora hover:text-blue-600">Contact Me</a>
+       </header>
+    </main>
+    </body>
+    
+    
+  );
+};
+
+export default HeroSection;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ export default {
   content: [ "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: ["Lora"],
+        royale: ["Bonheur Royale"],
+    },
   },
   plugins: [],
+}
 }
 
